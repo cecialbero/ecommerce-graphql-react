0197bccc2e20c7fe3372d2370aad39c23511e14a
cecialbero <ceci.albero@gmail.com>
2022-09-30 18:41:36 -0300

    Create a responsive Row

--- styles/Grid.tsx
+++ styles/Grid.tsx
@@ -1,4 +1,9 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components';
+
+type Props = {
+    md?: number;
+    sm?: number;
+}
 
 export const Container = styled.div`
     width: 100%;
@@ -8,8 +13,20 @@ export const Container = styled.div`
     box-sizing: border-box;
 `
 
-export const Col = styled.div`
+export const Row = styled.div<Props>`
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
-    grid-gap: 40px;
+    grid-template-columns: repeat(1, 1fr);
+    grid-gap: 15px;
+
+    ${({md, sm}) => css`
+        @media(min-width: 567px) {
+            grid-template-columns: repeat(${md}, 1fr);
+            grid-gap: 20px;
+        }
+
+        @media(min-width: 767px) {
+            grid-template-columns: repeat(${sm}, 1fr);
+            grid-gap: 30px;
+        }
+    `};
 `
