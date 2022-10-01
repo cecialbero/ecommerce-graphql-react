ec65062f3a68a5182934b5d836b0d7f9b759d709
cecialbero <ceci.albero@gmail.com>
2022-10-01 19:07:22 -0300

    Improve favicon resolution and change link tag into _app

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ const Document = () => {
     return (
         <Html>
             <Head>
-                <title>Shop App</title>
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
             </Head>
             <body>
